c39299789d24322e9e8c50127f1d027b401f8022
Victor M Hdez <vctr.hdez@gmail.com>
2025-01-31 20:08:57 +0100

    ventana primera

---
+++ app/src/main/java/com/example/login_kotlin/utils/showDialog.kt
@@ -0,0 +1,44 @@
+package com.example.login_kotlin.utils
+
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.login_kotlin.databinding.ActivityMainBinding
+
+class AlertDialogConstants {
+    companion object {
+        const val BTN_OK = 0
+        const val BTN_FALSE = 1
+        const val BTN_NEUTRAL = 2
+    }
+}
+
+fun AppCompatActivity.showDialog(contexto: ActivityMainBinding.Context, titulo: String, mensaje: String, btnOK: String?, btnFalse: String?, btnNeutro: String?, onButtonClick:(Int) -> Unit) {
+    // Crear un builder de AlertDialog
+    val builder = AlertDialog.Builder(contexto)
+
+    builder.setTitle(titulo) // Título del diálogo
+        .setMessage(mensaje) // Mensaje
+        .setCancelable(false) // No se puede cancelar tocando fuera del diálogo
+
+    if (btnOK != null) {
+        builder.setPositiveButton(btnOK) { dialog, which ->
+            onButtonClick(AlertDialogConstants.BTN_OK)
+        }
+    }
+
+    if (btnFalse != null) {
+        builder.setNegativeButton(btnFalse) { dialog, which ->
+            onButtonClick(AlertDialogConstants.BTN_FALSE)
+        }
+    }
+
+    if (btnFalse != null) {
+        builder.setNeutralButton(btnFalse) { dialog, which ->
+            onButtonClick(AlertDialogConstants.BTN_NEUTRAL)
+        }
+    }
+
+    // Mostrar el diálogo
+    val alertDialog = builder.create()
+    alertDialog.show()
+}
